55e71863ed4e43627ae87fbe67272a2e63e7c5c8
danielmishan85 <danielmishan85@gmail.com>
2022-06-22 00:35:07 +0300

    building wrapper components with props.children

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import { Route, Routes } from 'react-router-dom';
-import MainNavigation from './components/layout/MainNavigation';
+import Layout from './components/layout/Layout';
 import AllMeetupsPage from './pages/AllMeetups';
 import FavoritesPage from './pages/Favorites';
 import NewMeetupPage from './pages/NewMeetup';
 
 function App() {
   return (
-    <div>
-      <MainNavigation />
+    <Layout>
       <Routes>
         <Route path='/' element={<AllMeetupsPage />} />
         <Route path='/new-meetup' element={<NewMeetupPage />} />
         <Route path='/favorites' element={<FavoritesPage />} />
       </Routes>
-    </div>
+    </Layout>
   );
 }
 
